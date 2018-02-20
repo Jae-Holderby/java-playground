03c81ed1e3e4e3a9e3078035a505331df2fcc144
Jae-Holderby <jae.holderby@gmail.com>
2018-02-20 14:51:47 -0700

    Created JavaObjectModelExercise class, passed all tests

---
+++ src/main/java/com/galvanize/JavaObjectModelExercise.java
@@ -0,0 +1,43 @@
+package com.galvanize;
+
+
+
+class User implements Comparable, Cloneable {
+
+}
+
+class Employee extends User {
+
+}
+
+class Manager extends Employee {
+
+}
+
+class Customer extends User {
+
+}
+
+public class JavaObjectModelExercise {
+
+    boolean isUser(Object o){
+        return (o instanceof User);
+    }
+
+    boolean isEmployee(Object o){
+        return (o instanceof Employee);
+    }
+
+    boolean isManager(Object o){
+        return (o instanceof Manager);
+    }
+
+    boolean isCustomer(Object o){
+        return (o instanceof Customer);
+    }
+
+    boolean isComparable(Object o){
+        return (o instanceof Comparable);
+    }
+
+}
