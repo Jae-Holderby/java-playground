af575a2f8d987e26f6b53af2094a81fa74ff72ad
Jae-Holderby <jae.holderby@gmail.com>
2018-02-20 11:36:10 -0700

    refactored file structure, added IfElseexercise class

---
+++ src/main/java/com/galvanize/IfElseExercise.java
@@ -0,0 +1,41 @@
+package com.galvanize;
+
+public class IfElseExercise {
+
+    boolean isActive(String status) {
+
+        if(status.equals("active")){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    String grade(int input){
+
+        if(input >= 90){
+            return "A";
+        } else if (input >= 80 && input < 90) {
+            return "B";
+        } else if (input >= 70 && input < 80) {
+            return "C";
+        } else if (input >= 60 && input < 70) {
+            return "D";
+        } else {
+            return "F";
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        IfElseExercise thing = new IfElseExercise();
+
+        System.out.println(thing.isActive("active"));
+
+        System.out.println(thing.grade(60));
+
+    }
+
+}
