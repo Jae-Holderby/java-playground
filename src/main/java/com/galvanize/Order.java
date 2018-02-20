e7fe0b3d5cdc9076408d0dbe017e455d58382c0c
Jae-Holderby <jae.holderby@gmail.com>
2018-02-20 13:33:17 -0700

    Order exercise, tests pass

--- src/main/java/com/galvanize/Order.java
+++ src/main/java/com/galvanize/Order.java
@@ -7,7 +7,11 @@ public class Order {
     private String name;
     private LocalDateTime createdDate;
 
-    //your code goes here, do not change any existing code
+    public Order(String orderNumber, String name) {
+        this.orderNumber = orderNumber;
+        this.name = name;
+        this.createdDate = LocalDateTime.now();
+    }
 
     public String getOrderNumber() {
         return orderNumber;
